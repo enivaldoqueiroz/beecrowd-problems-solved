4759734ed502027e02dd2375e85ac486393e47c3
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-01 12:48:56 -0300

    1004 Produto Simples

---
+++ Beginner/1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine().Trim());
+            int b = Int32.Parse(Console.ReadLine().Trim());
+
+            int PROD = a * b;
+
+            Console.Write("PROD = {0}\n", PROD);            
+        }
+    }
+}
