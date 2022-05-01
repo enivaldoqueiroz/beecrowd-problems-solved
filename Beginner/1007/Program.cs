f7473a69ee06b78926e0c103a15094370969b9be
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-01 18:59:22 -0300

    1007 - Diferença

---
+++ Beginner/1007/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine(),CultureInfo.InvariantCulture);
+            int B = Convert.ToInt32(Console.ReadLine(),CultureInfo.InvariantCulture);
+            int C = Convert.ToInt32(Console.ReadLine(),CultureInfo.InvariantCulture);
+            int D = Convert.ToInt32(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            int DIFERENCA = ((A * B) - (C * D));
+
+            Console.Write("DIFERENCA = {0}\n", DIFERENCA.ToString(CultureInfo.InvariantCulture));
+
+        }
+    }
+}
