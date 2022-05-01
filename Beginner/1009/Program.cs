b152f366c8fbd679395b421a599a52b851d58026
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-01 19:52:58 -0300

    1009 - Salário com Bônus

---
+++ Beginner/1009/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeVendedor = Console.ReadLine();
+            double salarioFixo = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalDeVendaNoMes = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalAReceberNoFinalDoMes = salarioFixo + (totalDeVendaNoMes * 0.15);
+
+            Console.Write("TOTAL = R$ {0}\n", totalAReceberNoFinalDoMes.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
