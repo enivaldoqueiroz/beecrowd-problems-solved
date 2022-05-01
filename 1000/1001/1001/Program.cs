4cb644a31345401059ca107dc4de38b80895855a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-04-30 22:00:54 -0300

    1001 - Extremamente Básico

---
+++ 1000/1001/1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Int32.Parse(System.Console.ReadLine().Trim());
+            int B = Int32.Parse(System.Console.ReadLine().Trim());
+
+            int X = A + B;
+
+            Console.Write("X = {0}\n", X);
+        }
+    }
+}
