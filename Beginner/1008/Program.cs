11c13026bc29aa866c4418426d94757e0efec270
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-01 19:16:52 -0300

    1008 - Salário

---
+++ Beginner/1008/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int numeroHorasTrabalho = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double valorReceberPorHoraTrabalho = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salarioFuncionario = numeroHorasTrabalho * valorReceberPorHoraTrabalho;
+
+            Console.Write("NUMBER = {0}\n", numero);
+            Console.Write("SALARY = U$ {0}\n", salarioFuncionario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
