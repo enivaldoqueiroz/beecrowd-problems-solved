ae091788a528cf74d2b091794207415ff72db603
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-01 00:47:35 -0300

    Ajuste e implementação das soluções para Hello World!, Extremamente Básico e Área do Círculo

---
+++ Beginner/1002/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int B = Int32.Parse(System.Console.ReadLine().Trim());
+
+            //int X = A + B;
+
+            double n = 3.14159;
+            double raio = Double.Parse(System.Console.ReadLine().Trim());
+            double area = n * Math.Pow(raio,2);
+            string valueArea = area.ToString(".0000");
+            Console.Write("A = {0}\n", valueArea);
+        }
+    }
+}
