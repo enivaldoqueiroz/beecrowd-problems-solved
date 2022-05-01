608596ddc5f24ded1e19d9025e95d75925f644f6
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-01 00:50:18 -0300

    Ajuste no projeto 1002

--- Beginner/1002/Program.cs
+++ Beginner/1002/Program.cs
@@ -7,10 +7,6 @@ namespace _1002
     {
         static void Main(string[] args)
         {
-            //int B = Int32.Parse(System.Console.ReadLine().Trim());
-
-            //int X = A + B;
-
             double n = 3.14159;
             double raio = Double.Parse(System.Console.ReadLine().Trim());
             double area = n * Math.Pow(raio,2);
