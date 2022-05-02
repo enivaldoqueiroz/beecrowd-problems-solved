9a838f37cfbe1dfc08944dd132645d3731b4e2af
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-02 19:11:53 -0300

    1011 - Esfera

---
+++ Beginner/1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double PI = 3.14159;
+            double volumeRaio = (4.0/3) * PI * Math.Pow(R,3);
+
+            Console.Write("VOLUME = {0}\n", volumeRaio.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
