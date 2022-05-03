dca984abfcda4d5fc0dd4e275ce88534279d29e8
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-02 22:05:27 -0300

    1013 - O Maior

--- Beginner/1013/Program.cs
+++ Beginner/1013/Program.cs
@@ -6,6 +6,21 @@ namespace _1013
     {
         static void Main(string[] args)
         {
+            /*
+             URI Online Judge | 1013 - O Maior
+
+             Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da
+             mensagem “eh o maior”. Utilize a fórmula: (a + b + abs(a - b)) / 2
+
+            Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). 
+            Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+            Entrada
+                O arquivo de entrada contém três valores inteiros.
+
+            Saída
+                Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+             */
             int a, b, c;
 
             string[] vetorDeValoresEmLinha = Console.ReadLine().Split();
