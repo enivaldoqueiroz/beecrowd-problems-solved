a838b1151e6d2e4092109ca6d0f6253a59c3f03f
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-04 21:39:43 -0300

    1014 - Consumo

---
+++ Beginner/1014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double X = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double Y = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumoMedio = X / Y;
+
+            Console.Write("{0} km/l\n", consumoMedio.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
