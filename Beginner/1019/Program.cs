547a8973b7a429fd505eae867de949f7ed1f43c3
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-09 16:42:35 -0300

    1019 - Conversão de Tempo

---
+++ Beginner/1019/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Conversão de Tempo - beecrowd | 1019
+            
+             Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica,
+             e informe-o expresso no formato horas:minutos:segundos.
+
+             Entrada
+             O arquivo de entrada contém um valor inteiro N.
+
+             Saída
+             Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, 
+             conforme exemplo fornecido.
+             */
+
+            int valorEmSegundos = int.Parse(Console.ReadLine());
+
+            int segundos = valorEmSegundos % 60;
+            int minutos = valorEmSegundos / 60;
+            int horas = minutos / 60;
+            minutos = minutos % 60;
+
+            Console.WriteLine("{0}:{1}:{2}\n", horas, minutos, segundos);
+
+        }
+    }
+}
