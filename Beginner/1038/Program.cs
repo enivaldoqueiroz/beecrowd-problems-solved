415707ab8a6ffb09b41f0730c1ebe5f934bce487
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-14 14:14:39 -0300

    1038 - Lanche

---
+++ Beginner/1038/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo, quantidade;
+
+            string[] vetValores = Console.ReadLine().Split(' ');
+            codigo = int.Parse(vetValores[0]);
+            quantidade = int.Parse(vetValores[1]);
+
+            double valorTotal = 0;
+            switch (codigo)
+            {
+                case 1:
+                    valorTotal = 4.00 * quantidade;
+                    Console.WriteLine("Total: R$ {0}", valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 2:
+                    valorTotal = 4.50 * quantidade;
+                    Console.WriteLine("Total: R$ {0}", valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 3:
+                    valorTotal = 5.00 * quantidade;
+                    Console.WriteLine("Total: R$ {0}", valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 4:
+                    valorTotal = 2.00 * quantidade;
+                    Console.WriteLine("Total: R$ {0}", valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 5:
+                    valorTotal = 1.50 * quantidade;
+                    Console.WriteLine("Total: R$ {0}", valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+            }
+        }
+    }
+}
