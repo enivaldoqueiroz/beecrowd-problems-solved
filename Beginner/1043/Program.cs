04c5ee0fb4bf6c1eed38130e1f99229ee6a72941
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-05-15 11:15:08 -0300

    1043 - Triângulo

---
+++ Beginner/1043/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            double perimetroDoTriangolo = a + b + c;
+            double areaDoTrapezio = ((a + b) * c) / 2;
+
+            if ((a < (b + c)) && (b < (a + c)) && (c < (b + a)))
+                Console.WriteLine("Perimetro = {0}", perimetroDoTriangolo.ToString("F1", CultureInfo.InvariantCulture));
+            else
+                Console.WriteLine("Area = {0}", areaDoTrapezio.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
